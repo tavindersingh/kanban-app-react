6953f2ea3651cb1dfbc2975fb4b2798a33c1550a
Tavinder Singh <tavinder123singh@gmail.com>
2024-07-30 13:29:17 +0530

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#635fc5",
+      secondary: "#4d4aa1",
+      dark: "#4d4aa1",
+      dark2: "#2c2c37",
+      "primary-text": "#eeeff3",
+      "secondary-text": "#656482",
+      pending: "#47c2e6",
+      progress: "#47c2e6",
+      done: "#6ae6b0",
+    },
   },
   plugins: [],
 };
